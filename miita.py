7025ea1262506306ea500d5427c29ff4d9ad7c5d
INADA Naoki <inada-n@klab.com>
2012-11-30 12:13:25 +0900

    cleanup

--- miita.py
+++ miita.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask.ext.pymongo import PyMongo
 from flask.ext.googleauth import (GoogleFederated, GoogleAuth)
 import markdown
+import datetime
 
 
 app = Flask(__name__)
@@ -12,18 +13,23 @@ mongo = PyMongo(app)
 auth = GoogleFederated(app, 'klab.com')
 #auth = GoogleAuth(app)
 
+
 @app.route('/')
 @auth.required
 def index():
     articles = mongo.db.articles.find().limit(10).sort('_id', -1)
-    return flask.render_template('index.html', articles=articles, user=flask.g.user)
+    return flask.render_template('index.html',
+                                 articles=articles,
+                                 user=flask.g.user)
 
 
 @app.route('/article/<ObjectId:article_id>')
 @auth.required
 def article(article_id):
     article = mongo.db.articles.find_one_or_404(article_id)
-    return flask.render_template('article.html', article=article, user=flask.g.user)
+    return flask.render_template('article.html',
+                                 article=article,
+                                 user=flask.g.user)
 
 
 #@app.route('/edit/<ObjectId:article_id>')
@@ -35,10 +41,12 @@ def edit(article_id=None):
     else:
         article = mongo.db.articles.find_one_or_404(article_id)
         source = article.source
-    return flask.render_template('edit.html', source=source, user=flask.g.user)
+    return flask.render_template('edit.html',
+                                 source=source,
+                                 user=flask.g.user)
 
 
-@app.route('/post', methods=['POSt'])
+@app.route('/post', methods=['POST'])
 @auth.required
 def post():
     article_id = flask.request.form.get('article')
@@ -50,13 +58,17 @@ def post():
 
     if article_id:
         article = mongo.db.articles.find_one_or_404(article_id)
-        #todo: 編集権チェック
+        if article.get('author-email') != flask.g.user['email']:
+            flask.abort(403)
     else:
         article = {}
     article['source'] = source
     article['html'] = html
+    article['title'] = flask.request.form.get('title')
+    article['author-name'] = flask.g.user['name']
+    article['author-email'] = flask.g.user['email']
+    article['last-update'] = datetime.datetime.utcnow()
     wrote_id = mongo.db.articles.save(article)
-    print wrote_id
     return flask.redirect(flask.url_for('article', article_id=wrote_id))
 
 if __name__ == '__main__':
