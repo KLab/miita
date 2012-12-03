9ee14cac128522814e772ed72b96d0cc6f8cf95e
INADA Naoki <inada-n@klab.com>
2012-12-04 00:01:31 +0900

    User.email を primary_key に.

--- miita/models.py
+++ miita/models.py
@@ -3,14 +3,14 @@ from .application import mongo as m
 
 
 class User(m.Document):
-    email = m.StringField()
+    email = m.StringField(primary_key=True)
     name = m.StringField()
 
 
 class Item(m.Document):
-    source = m.StringField()
-    html = m.StringField()
-    title = m.StringField()
+    source = m.StringField(default='')
+    html = m.StringField(default='')
+    title = m.StringField(default='')
     author = m.ReferenceField(User, dbref=True)
     updated_at = m.DateTimeField()
     tags = m.SortedListField(m.StringField())
